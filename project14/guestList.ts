9de8ce5abfa6fb06aa70355ec5ba03263f7819ec
AliHaider3820 <125271803+AliHaider3820@users.noreply.github.com>
2023-05-03 00:26:18 +0500

    Add files via upload

---
+++ project14/guestList.ts
@@ -0,0 +1,13 @@
+/*
+Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+Make a list that includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, inviting them to dinner.
+*/
+
+export{};
+let gustName = ["Talha", "Ishaq", "Usama", "Faiez"];
+const Message=", I am inviting you at dinner. ";
+console.log(`${gustName[0]}` + Message);
+console.log(`${gustName[1]}` + Message);
+console.log(`${gustName[2]}` + Message);
+console.log(`${gustName[3]}` + Message);
